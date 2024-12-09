77d275782465171afe04db00ec436951d6efb548
Hồ Nhân <104899198+NhanHoPeter217@users.noreply.github.com>
2024-12-10 01:02:48 +0700

    update api.ts

--- frontend/car-inspection-frontend/src/api.ts
+++ frontend/car-inspection-frontend/src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BACKEND_URL; // Adjust the URL as needed
+console.log("BASE_URL", BASE_URL);
 
 export const getCars = async () => {
   try {
